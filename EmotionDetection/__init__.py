5fe47d1a9ede4305bf2fef923d8b46276f1a3b9e
Pranay-Siddhartha <76801587+Pranay-Siddhartha@users.noreply.github.com>
2026-03-15 15:09:56 +0530

    Remove docstring from EmotionDetection package
    Removed package documentation from __init__.py

--- EmotionDetection/__init__.py
+++ EmotionDetection/__init__.py
@@ -1,12 +1,4 @@
-"""EmotionDetection package.
 
-This package provides a simple wrapper around emotion detection utilities.
-
-It exposes a single function:
-
-- `emotion_detector(text: str) -> str`
-
-"""
 
 from .emotion_detection import emotion_detector
 
